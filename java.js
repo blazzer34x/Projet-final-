a7c6699565674b05c438f064f33ae235204dfb80
BlazzerOnTheTrack13 <136841379+blazzer34x@users.noreply.github.com>
2025-06-28 10:45:11 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+<script>
+    var toggle_menu = document.querySelector('.responsive-menu');
+    var menu = document.querySelector('.menu');
+    toggle_menu.onclick= function(){
+         toggle_menu.classList.toggle('active');
+         menu.classList.toggle('responsive')
+    }
+</script>
